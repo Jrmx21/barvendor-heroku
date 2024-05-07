a4cf4fb873cedee6f64a36ef53e7edcaeef7314c
David Ruiz Peña <119055850+Jrmx21@users.noreply.github.com>
2024-05-07 14:38:10 +0200

    funsiona

--- src/main/java/com/davidruiz/barvendor/Tables/TableController.java
+++ src/main/java/com/davidruiz/barvendor/Tables/TableController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/tables")
+@RequestMapping("/mesas")
 public class TableController {
 
     @Autowired
@@ -27,11 +27,13 @@ public class TableController {
         model.addAttribute("table", new TableModel());
         return "crearMesa"; // Nombre de la vista de creación de mesa
     }
+
     @PostMapping("/crear")
     public String createTable(@ModelAttribute("table") TableModel table) {
         tableService.createTable(table);
-        return "redirect:/tables/listar";
+        return "redirect:/mesas/listar";
     }
+
     @GetMapping("/editar/{id}")
     public String showEditTableForm(@PathVariable("id") Long id, Model model) {
         TableModel table = tableService.getTableById(id);
@@ -42,13 +44,13 @@ public class TableController {
     @PostMapping("/editar")
     public String editTable(@ModelAttribute("table") TableModel table) {
         tableService.updateTable(table);
-        return "redirect:/tables/listar";
+        return "redirect:/mesas/listar";
     }
 
     @GetMapping("/eliminar/{id}")
     public String deleteTable(@PathVariable("id") Long id) {
         tableService.deleteTable(id);
-        return "redirect:/tables/listar";
+        return "redirect:/mesas/listar";
     }
 
 }
