f078542de15b0bb7f572e64431873bf3a58e517a
David Ruiz Peña <119055850+Jrmx21@users.noreply.github.com>
2024-05-21 18:39:18 +0200

    logueo por admin

--- src/main/java/com/davidruiz/barvendor/Security/SecurityConfig.java
+++ src/main/java/com/davidruiz/barvendor/Security/SecurityConfig.java
@@ -28,8 +28,8 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable()) // Disable CSRF protection if needed
             .authorizeRequests(authorizeRequests -> authorizeRequests
                 .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll() // Permitir acceso a estas rutas
-                .requestMatchers("/api/**").hasAnyRole("CAMARERO", "COCINA")
-                .anyRequest().authenticated() // Cualquier otra solicitud debe estar autenticada
+                .requestMatchers("/api/**").permitAll() // Permitir acceso a todas las APIs
+                .anyRequest().hasRole("Admin") // Restringir acceso a cualquier otra solicitud solo para administradores
             )
             .formLogin(formLogin -> formLogin
                 .loginPage("/login")
