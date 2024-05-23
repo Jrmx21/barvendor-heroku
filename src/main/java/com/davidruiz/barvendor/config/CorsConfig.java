d1ac70faca934b7aba9a773606a162a1660d4bd4
David Ruiz Peña <119055850+Jrmx21@users.noreply.github.com>
2024-05-23 11:00:07 +0200

    AHORA SI

--- src/main/java/com/davidruiz/barvendor/config/CorsConfig.java
+++ src/main/java/com/davidruiz/barvendor/config/CorsConfig.java
@@ -10,10 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*") // Aquí puedes agregar los orígenes permitidos
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
-            .allowCredentials(true)
+            .allowCredentials(false)
             .maxAge(3600);
     }
 }
