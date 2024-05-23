0385d0092fd6b9ef6c6e52be5e348e70b20f5186
David Ruiz Peña <119055850+Jrmx21@users.noreply.github.com>
2024-05-23 10:16:37 +0200

    pre despliegue

--- src/main/java/com/davidruiz/barvendor/Security/SecurityConfig.java
+++ src/main/java/com/davidruiz/barvendor/Security/SecurityConfig.java
@@ -10,7 +10,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -28,19 +31,23 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable()) // Disable CSRF protection if needed
             .authorizeRequests(authorizeRequests -> authorizeRequests
                 .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll() // Permitir acceso a estas rutas
-                .requestMatchers("/api/**").permitAll()
-                .requestMatchers("/api/login").permitAll()  // Permitir acceso a todas las APIs
-                .anyRequest().hasRole("Admin") // Restringir acceso a cualquier otra solicitud solo para administradores
+                .requestMatchers("/api/**").permitAll()  // Permitir acceso a todas las APIs
+                .requestMatchers("/kitchen/orders").hasRole("Cocina") // Permitir acceso a /kitchen/orders solo a Cocina
+                .requestMatchers("/").hasRole("Admin") // Permitir acceso a / solo a Admin
+                .anyRequest().authenticated() // Restringir acceso a cualquier otra solicitud autenticada
             )
             .formLogin(formLogin -> formLogin
                 .loginPage("/login")
-                .defaultSuccessUrl("/", true)
+                .successHandler(customAuthenticationSuccessHandler()) // Usar customAuthenticationSuccessHandler para redirigir según el rol
                 .permitAll()
             )
             .logout(logout -> logout
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login?logout")
                 .permitAll()
+            )
+            .exceptionHandling(exceptionHandling -> exceptionHandling
+                .accessDeniedPage("/access-denied")
             );
 
         return http.build();
@@ -58,5 +65,22 @@ public class SecurityConfig {
         authenticationManagerBuilder.userDetailsService(userDetailsService)
                                     .passwordEncoder(passwordEncoder());
         return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return (request, response, authentication) -> {
+            authentication.getAuthorities().forEach(grantedAuthority -> {
+                try {
+                    if (grantedAuthority.getAuthority().equals("ROLE_Cocina")) {
+                        response.sendRedirect("/kitchen/orders");
+                    } else if (grantedAuthority.getAuthority().equals("ROLE_Admin")) {
+                        response.sendRedirect("/");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        };
     }
 }
